27604b4ca81bfcac45ae2718af8d1060f5370aca
ethan.payne <epay1@student.monash.edu>
2025-01-17 14:10:46 -0800

    added detector and signal classes

---
+++ src/GWPhotonCounting/detector.py
@@ -0,0 +1,98 @@
+from .utils import lorentzian_complex, load_and_interpolate_data, gram_schmidt
+import jax.numpy as jnp
+import jax
+import numpy as np
+
+import bilby
+
+
+class Detector:
+    def __init__(self, frequencies, shot_noise_psd, classical_noise_quanta, ifo_name='CE',
+                 base_filter_function=lorentzian_complex, N_frequency_spaces=15,
+                 minimum_frequency=1.5e3, maximum_frequency=4e3, maximum_duration=4e-2,
+                 random_seed=1234, gaussian_noise=False, **kwargs):
+        # Load and interpolate shot noise PSD data
+        self.shot_noise_psd = load_and_interpolate_data(shot_noise_psd, frequencies)
+
+        # Load and interpolate classical noise quanta data, then transform it back into a PSD
+        classical_noise_quanta_psd = load_and_interpolate_data(classical_noise_quanta, frequencies)
+        self.classical_noise_psd = classical_noise_quanta_psd * 2 * self.shot_noise_psd
+        
+        # Setting up the spacing of the temporal mode filters
+        self.N_total_filters = int(2 * maximum_duration * (maximum_frequency - minimum_frequency))
+        self.N_time_spaces = int(self.N_total_filters/N_frequency_spaces/2)
+        self.N_frequency_spaces = N_frequency_spaces
+        
+        self.f0_values = jnp.linspace(minimum_frequency, maximum_frequency, N_frequency_spaces)
+        self.t0_values = jnp.linspace(-maximum_duration/2, maximum_duration/2, self.N_time_spaces)
+
+        # Construct the arrays of filter functions
+        self.filter_functions, self.filter_labels = self._construct_filter_functions(
+            frequencies, base_filter_function, random_seed, **kwargs
+        )
+        
+        # Setting up the interferometer
+        self.ifo = bilby.gw.detector.FrequencyDependentInterferometer.from_name(ifo_name, time_dependent=False)
+        self.ifo.power_spectral_density = bilby.gw.detector.PowerSpectralDensity.from_power_spectral_density_array(
+            frequencies, np.array(self.shot_noise_psd+self.classical_noise_psd))
+        
+        if gaussian_noise is False:
+            self.ifo.set_strain_data_from_zero_noise(sampling_frequency=2*frequencies[-1], duration=1/(frequencies[1]-frequencies[0]), start_time=0)
+        elif gaussian_noise is True:
+            self.ifo.set_strain_data_from_power_spectral_density(sampling_frequency=2*frequencies[-1], duration=1/(frequencies[1]-frequencies[0]), start_time=0)
+
+        # Compute the number of expected photons from the noise background here
+        self.noise_photon_expectation = self.calculate_noise_photon_expectation(frequencies)
+
+    def _construct_filter_functions(self, frequencies, filter_function,
+                                    random_seed, **kwargs):
+        """Helper method to construct filter functions."""
+        
+        
+        filter_functions_at_t0 = jnp.concatenate([
+            jnp.array([filter_function(frequencies, f0=f0, A=1, phase=0, **kwargs) for f0 in self.f0_values]),
+            jnp.array([filter_function(frequencies, f0=f0, A=1, phase=jnp.pi/2, **kwargs) for f0 in self.f0_values])])
+        
+        filter_labels_f0_t0 = [(f0, t0) for f0 in self.f0_values for t0 in self.t0_values]
+
+        filter_labels = []
+
+        for phase in (0, jnp.pi/2):
+            filter_labels += [(float(f0), float(t0), phase) for f0, t0 in filter_labels_f0_t0]
+
+        time_delays = jnp.exp(-2j * jnp.pi * jnp.outer(self.t0_values, frequencies))
+        
+        filter_functions = jnp.einsum('ij, kj -> ikj', filter_functions_at_t0, time_delays).reshape(-1, len(frequencies))
+        
+        key = jax.random.PRNGKey(random_seed)
+        shuffled_indices = jax.random.permutation(key, self.N_total_filters)
+        orthonormalized_filters = gram_schmidt(filter_functions[shuffled_indices])/jnp.sqrt(frequencies[1] - frequencies[0])
+
+        shuffled_filter_labels = []
+        for i in shuffled_indices:
+            shuffled_filter_labels.append(filter_labels[i])
+
+        return orthonormalized_filters, shuffled_filter_labels
+    
+    def calculate_signal_photon_expectation(self, strain, frequencies):
+        
+        quanta_amplitude = strain/jnp.sqrt(2 * self.shot_noise_psd)
+
+        integral = jnp.einsum('...k, jk -> ...j', jnp.conj(quanta_amplitude), self.filter_functions * jnp.diff(frequencies, append=frequencies[-1]))
+
+        signal_photon_expectation = jnp.abs(integral)**2
+        
+        return signal_photon_expectation
+
+    def calculate_noise_photon_expectation(self, frequencies):
+        noise_photon_expectation = jnp.zeros(self.N_total_filters, dtype=float)
+
+        noise_quanta_psd = self.classical_noise_psd/(2 * self.shot_noise_psd)
+
+        for i in range(self.N_total_filters):
+            noise_photon_expectation = noise_photon_expectation.at[i].set(
+                jnp.sum(noise_quanta_psd * jnp.abs(self.filter_functions[i])**2 * jnp.diff(frequencies, append=frequencies[-1]))
+            )
+
+        return noise_photon_expectation
+    
---
+++ src/GWPhotonCounting/signal.py
@@ -0,0 +1,94 @@
+import jax.numpy as jnp
+import numpy as np
+import jax
+import pickle as pkl
+import bilby
+from scipy.interpolate import interp1d
+from astropy.cosmology import Planck18
+
+from scipy.signal import savgol_filter
+from scipy.signal import find_peaks
+
+from .utils import phase_shift, lorentzian_complex
+
+# Constants
+CU_to_ms = 0.004925 # Geom to ms
+
+class BasePhotonCountingSignal:
+
+    def __init__(self):
+        pass
+
+    def generate_strain(self, detector, frequencies, **kwargs):
+        """This needs to be overwritten by the subclass."""
+        pass
+
+    def generate_photon_count(self, detector, frequencies, **kwargs):
+        
+        strain = self.generate_strain(detector, frequencies, **kwargs)
+
+        photon_count_expectation = detector.calculate_signal_photon_expectation(strain, frequencies)
+
+        return photon_count_expectation
+
+class PostMergerKNN(BasePhotonCountingSignal):
+    '''
+    Class using the KNN post-merger model from XXX to generate the strain signal. We'll convert into photons 
+    using the detector class later. 
+    '''
+    
+    def __init__(self, ti=0, tf=20, dt=5, knn_file_path='/home/ethan.payne/code_libraries/apr4_knn_gw_model_2024/KNN_Models/APR4-knn_model-N100'):
+        with open(knn_file_path, 'rb') as file:
+            self.KNN_model = pkl.load(file)
+        
+        self.dt_ms = dt * CU_to_ms
+        self.time_array = jnp.arange(ti, tf, self.dt_ms)
+    
+    def generate_plus_cross_strain(self, frequencies, mtot, phi0, z):
+        
+        mtot_arr = jnp.zeros(self.time_array.size) + mtot
+        
+        # Generatinging the hplus signal from the KKN model - note the complicated factor for scaling to the correct distance
+        hplus = self.KNN_model.predict(jnp.transpose(jnp.vstack((mtot_arr, self.time_array)))) * (1+z)/Planck18.luminosity_distance(z).value / 3.085677581491367e+22 * mtot * 2e30 * 6.67e-11/(3e8)**2
+
+        
+        hplus =  phase_shift(hplus, angle = 2*phi0)
+        hcross = phase_shift(hplus, angle = 2*phi0+jnp.pi/2)
+
+        i_array = np.array(range(len(self.time_array)))
+        n_taper_start = (find_peaks(savgol_filter(np.abs(hplus)[:1000], 25,3))[0][4] + find_peaks(savgol_filter(np.abs(hcross)[:500], 25,3))[0][4])/2
+
+        with np.errstate(divide='ignore'):
+            z_start = (n_taper_start/i_array) + n_taper_start/(i_array - n_taper_start)
+        sigma = 1/(np.exp(z_start)+1) *(i_array < n_taper_start) + 1*(i_array >= n_taper_start)
+        
+        hp_fd, freq_array = bilby.core.utils.nfft(hplus * sigma, sampling_frequency=1e3/(self.dt_ms))
+        hc_fd, freq_array = bilby.core.utils.nfft(hcross * sigma, sampling_frequency=1e3/(self.dt_ms))
+        
+        return interp1d(jnp.concatenate((freq_array/(1+z), -freq_array[1:]/(1+z))), jnp.concatenate((hp_fd, jnp.conj(hp_fd[1:]))), kind='cubic')(frequencies), \
+            interp1d(jnp.concatenate((freq_array/(1+z), -freq_array[1:]/(1+z))), jnp.concatenate((hc_fd, jnp.conj(hc_fd[1:]))), kind='cubic')(frequencies)
+
+    def generate_strain(self, detector, frequencies, mtot, phi0, z, ra, dec, iota, psi):
+        
+        hp, hc = self.generate_plus_cross_strain(frequencies, mtot, phi0, z)
+
+        # Using Bilby to get the antenna response function for the frequency dependent detector behavior
+        h = detector.ifo.get_detector_response({'plus':np.array(hp * (1+jnp.cos(iota)**2)/2), 'cross':np.array(hc * jnp.cos(iota))}, 
+                                                {'ra':ra, 'dec':dec, 'psi':psi, 'iota':iota, 'geocent_time':0}, 
+                                                frequencies=np.array(frequencies, dtype=np.float64))
+
+        return jnp.nan_to_num(h)
+
+class PostMergerLorentzian(BasePhotonCountingSignal):
+
+    def __init__(self):
+        pass
+
+    def generate_strain(self, detector, frequencies, f0, gamma, A, phase, t0):
+
+        return jnp.atleast_2d(lorentzian_complex(frequencies, f0, gamma, A, phase) * \
+            jnp.exp(-2j * jnp.pi * jnp.outer(t0, frequencies)))
+    
+
+
+
